1e4d52f89643192e373ffa47064a16428ccdd5da
Harijaona <hravelondrina@gmail.com>
2023-02-20 17:04:00 +0100

    Main module's resouces

---
+++ main.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "remote_state" {
+  bucket = "${var.prefix}-remote-state-${var.environment}"
+  lifecycle {
+    prevent_destroy = true
+  }
+  versioning {
+    enabled = true
+  }
+  tags = {
+    Name        = "${var.prefix}-remote-state-${var.environment}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_acl" "remote_state_acl" {
+  bucket = aws_s3_bucket.remote_state.id
+  acl    = "authenticated-read"
+}
+
+resource "aws_s3_bucket_versioning" "remote_state_versioning" {
+  bucket = aws_s3_bucket.remote_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
